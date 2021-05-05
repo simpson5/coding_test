c4fb7fbafb85593c3fe999250a6944e8d2c672d0
simpson5 <450054@naver.com>
2021-05-05 19:37:52 +0900

    lv2. scoville 1차 정확성: 57.1 효율성: 0.0 합계: 57.1 / 100.0

--- Scoville/src/Solution.java
+++ Scoville/src/Solution.java
@@ -1,7 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+//섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
 public class Solution {
 	public int solution(int[] scoville, int K) {
 		int answer = 0;
+		List<Integer> list = new ArrayList<>();
+		for (int s : scoville) {
+			list.add(s);
+		}
+		while (true) {
+			boolean check = true;
+			for (int s : list) {
+				if (s < K) {
+					check = false;
+				}
+			}
+			if (check) {
+				break;
+			}
+
+			Collections.sort(list);
+
+			int k = list.get(0) + list.get(1) * 2;
+			list.remove(0);
+			list.remove(0);
+			list.add(k);
+
+			answer++;
+		}
 		return answer;
 	}
 }
