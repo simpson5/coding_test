a2129d4a0459813dec284ce5fb9c8eeaf6898140
simpson5 <450054@naver.com>
2021-05-05 15:46:40 +0900

    lv1.124Number 30%

---
+++ 124Number/src/Solution.java
@@ -0,0 +1,23 @@
+
+public class Solution {
+	public String solution(int n) {
+		// 11 12 14 21 22 24 41 42 44 111 112 114 121 122
+		// 4  5  6  7  8  9  10 11 12 13  14  15  16  17
+		String answer = "";
+		while(n > 3) {
+			int remainder = n % 3;
+			switch(remainder) {
+			case 1 :
+				answer = "1" + answer;
+				break;
+			case 2 :
+				answer = "2" + answer;
+				break;
+			case 0 :
+				answer = "4" + answer;
+				break;
+			}
+		}
+		return answer;
+	}
+}
