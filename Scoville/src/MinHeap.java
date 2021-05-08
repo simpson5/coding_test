325129e304856dd754873ef347fea295fc093de9
simpson5 <450054@naver.com>
2021-05-08 18:43:35 +0900

    lv2.scovill

--- Scoville/src/MinHeap.java
+++ Scoville/src/MinHeap.java
@@ -1,53 +1,20 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
-//섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
-public class Solution {
+class MinHeap {
+	private ArrayList<Integer> heap;
 
-	public int solution(int[] scoville, int K) {
-		int answer = 0;
-
-		ArrayList<Integer> heap;
+	/* heap init */
+	public MinHeap() {
 		heap = new ArrayList<>();
 		heap.add(0); // heap 의 인덱스는 알기 쉽게 0부터 시작한다는 특성을 따른다.
-
-		// heap 이진 트리 정렬
-		for (int s : scoville) {
-			insert(heap, s);
-		}
-
-		if (scoville[0] == 0 && scoville[1] == 0) {
-			return -1;
-		}
-
-		while (true) {
-			System.out.println(heap.get(1));
-			//더이상 진행할 수 없을때
-			if (heap.size() == 2 && heap.get(1) < K) {
-				return -1;
-			}
-
-			if (heap.get(1) >= K) {
-				return answer;
-			}
-
-			int x = heap.get(1);
-			delete(heap);
-			int y = heap.get(1);
-			delete(heap);
-			int k = x + y * 2;
-			insert(heap, k);
-			answer++;
-			System.out.println(answer + " : " + heap);
-		}
 	}
 
 	// insertion
-	private void insert(ArrayList<Integer> heap, int data) {
+	private void insert(int data) {
 		heap.add(data);
 		int position = heap.size() - 1;
 		// 루트 노드까지 이동했거나, 부모 Heap이 자식 Heap보다 작을 때 까지
-		while (position > 1 && heap.get(position / 2) > heap.get(position)) {
+		while (position > 1 && heap.get(position / 2) < heap.get(position)) {
 
 			/* 부모 노드와 자식 노드간의 swap을 위한 연산 */
 			int temp = heap.get(position / 2);
@@ -58,7 +25,7 @@ public class Solution {
 		}
 	}
 
-	int delete(ArrayList<Integer> heap) {
+	int delete() {
 		// heap 사이즈가 1보다 작으면 즉, 힙에 값이 없으면 return 0;
 		if (heap.size() - 1 < 1) {
 			return 0;
